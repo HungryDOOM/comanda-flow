c62bf558360dc4ff7e63c950283b0e3a119f743c
HungryDOOM <brianjoelam12_@hotmail.com>
2026-03-23 22:30:41 -0600

    Fix Backend service

--- src/services/api.ts
+++ src/services/api.ts
@@ -12,12 +12,12 @@ export interface Pedido {
 }
 
 export const getProductos = async (): Promise<Producto[]> => {
-  const response = await fetch(`http://localhost:3001/productos`);
+  const response = await fetch(`https://comanda-flow-bkn.onrender.com/productos`);
   return response.json();
 };
 
 export const sendPedido = async (mesa: number, pedido: string[], precio_total: number): Promise<Pedido[]> => { 
-  const response = await fetch(`http://localhost:3001/pedidos`, {
+  const response = await fetch(`https://comanda-flow-bkn.onrender.com/pedidos`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
@@ -28,6 +28,6 @@ export const sendPedido = async (mesa: number, pedido: string[], precio_total: n
 };
 
 export const getPedidos = async (): Promise<Pedido[]> => {
-  const response = await fetch(`http://localhost:3001/getPedidos`);
+  const response = await fetch(`https://comanda-flow-bkn.onrender.com/getPedidos`);
   return response.json();
 };
